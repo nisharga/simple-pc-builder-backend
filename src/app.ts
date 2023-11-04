1424301519bd2d561cb71dcfed8cd388dcbd5dd7
Nisharga Kabir <kabirnisharga@gmail.com>
2023-11-04 14:01:44 +0600

    cors added done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import express, { Application,Request, Response } from "express";
 const app: Application = express();
 const port = 3000
+
+app.use(cors())
+// parser
+app.use(express.json());
+app.use(express.urlencoded( {extended: true} ));
+
+
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
