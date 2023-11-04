6acd479c67bd3310f6ce5d998dcfbaa1b06d64b8
Nisharga Kabir <kabirnisharga@gmail.com>
2023-11-04 14:23:53 +0600

    initital setup done

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express, { Application,Request, Response } from "express";
 const app: Application = express();
-const port = 3000
+import cors from 'cors'; 
 
-app.use(cors())
+app.use(cors());
 // parser
 app.use(express.json());
 app.use(express.urlencoded( {extended: true} ));
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import config from "../config";
 import app from "./app";
 import mongoose from 'mongoose';
